fa755834f20bdd28d2e2d63fd85b62735274ba81
kenti <016mki69@gmail.com>
2024-10-25 02:57:38 -0300

    Feat: add some more projects to Project component

--- src/app/features/components/projects/projects.component.ts
+++ src/app/features/components/projects/projects.component.ts
@@ -25,6 +25,20 @@ export class ProjectsComponent {
       description: 'Built a Python-based API using Flask, integrating with external weather services. Implemented caching for improved performance and rate limiting for API security.',
       photoUrl: 'https://res.cloudinary.com/dpvs5m9an/image/upload/v1729831904/the-walking-dogs_kkdpq6.jpg',
       repoUrl: 'https://github.com/23737k/the-walking-dogs-app'
+    },
+    {
+      title: 'HealthTech',
+      subtitle: 'A telemedicine app for online consultations with certified doctors from home.',
+      description: 'Built a Python-based API using Flask, integrating with external weather services. Implemented caching for improved performance and rate limiting for API security.',
+      photoUrl: 'https://res.cloudinary.com/dpvs5m9an/image/upload/v1729835457/sore-throat-istock-1176_seywvy.jpg',
+      repoUrl: 'https://github.com/No-Country-simulation/c20-42-n-java-react'
+    },
+    {
+      title: 'My Carbon FootPrint',
+      subtitle: 'A carbon footprint calculator app to measure and reduce your environmental impact.',
+      description: 'Built a Python-based API using Flask, integrating with external weather services. Implemented caching for improved performance and rate limiting for API security.',
+      photoUrl: 'https://res.cloudinary.com/dpvs5m9an/image/upload/v1729835710/image_bbnohm.png',
+      repoUrl: 'https://github.com/23737k/mi-huella-de-carbono'
     }
   ]
 
