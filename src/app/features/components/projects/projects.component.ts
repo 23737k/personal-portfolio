38c460e27f15f1e1b25c794a020c8ea15ace1075
Kenti Antezana <mopup2017@gmail.com>
2024-10-25 14:58:39 -0300

    Feat: add about me section

--- src/app/features/components/projects/projects.component.ts
+++ src/app/features/components/projects/projects.component.ts
@@ -16,28 +16,28 @@ export class ProjectsComponent {
       title: 'My Fitness App',
       subtitle: 'A fitness web application for tracking workouts',
       description: 'Built a Python-based API using Flask, integrating with external weather services. Implemented caching for improved performance and rate limiting for API security.',
-      photoUrl: 'https://res.cloudinary.com/dpvs5m9an/image/upload/v1729814832/Fitness-for-10-Home-Licensing-Information_e1ekte.jpg',
+      photoUrl: 'https://res.cloudinary.com/dpvs5m9an/image/upload/q_auto:low/f_auto/q_70/Fitness-for-10-Home-Licensing-Information_e1ekte.jpg',
       repoUrl: 'https://github.com/23737k/myfitnessapp'
     },
     {
       title: 'The Walking Dogs',
       subtitle: 'Project for a Dog Walking and Care Services Application',
       description: 'Built a Python-based API using Flask, integrating with external weather services. Implemented caching for improved performance and rate limiting for API security.',
-      photoUrl: 'https://res.cloudinary.com/dpvs5m9an/image/upload/v1729831904/the-walking-dogs_kkdpq6.jpg',
+      photoUrl: 'https://res.cloudinary.com/dpvs5m9an/image/upload/f_auto/q_auto:low/q_70/the-walking-dogs_kkdpq6.jpg',
       repoUrl: 'https://github.com/23737k/the-walking-dogs-app'
     },
     {
       title: 'HealthTech',
       subtitle: 'A telemedicine app for online consultations with certified doctors from home.',
       description: 'Built a Python-based API using Flask, integrating with external weather services. Implemented caching for improved performance and rate limiting for API security.',
-      photoUrl: 'https://res.cloudinary.com/dpvs5m9an/image/upload/v1729835457/sore-throat-istock-1176_seywvy.jpg',
+      photoUrl: 'https://res.cloudinary.com/dpvs5m9an/image/upload/f_auto/q_auto:low/q_70/sore-throat-istock-1176_seywvy.jpg',
       repoUrl: 'https://github.com/No-Country-simulation/c20-42-n-java-react'
     },
     {
       title: 'My Carbon FootPrint',
       subtitle: 'A carbon footprint calculator app to measure and reduce your environmental impact.',
       description: 'Built a Python-based API using Flask, integrating with external weather services. Implemented caching for improved performance and rate limiting for API security.',
-      photoUrl: 'https://res.cloudinary.com/dpvs5m9an/image/upload/v1729835710/image_bbnohm.png',
+      photoUrl: 'https://res.cloudinary.com/dpvs5m9an/image/upload/f_auto/q_auto:low/q_70/image_bbnohm.png',
       repoUrl: 'https://github.com/23737k/mi-huella-de-carbono'
     }
   ]
