85d2a77d783eb5e70fc954ca738211df71abe538
Kenti <016mki69@gmail.com>
2024-10-26 14:34:46 -0300

    Refactor: change about-me text

--- src/app/features/components/header/header.component.ts
+++ src/app/features/components/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {NgClass} from "@angular/common";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(private _route: ActivatedRoute) {
+  }
 
 }
